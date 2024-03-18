f14a6367723b08520631faa6665e02d674a36207
Arsalan Mughal Tekrevol <arsalan.mughal@tekrevol.com>
2024-03-18 23:07:50 +0500

    Task 14 & 15 is Improved

--- task_14_and_15/index.js
+++ task_14_and_15/index.js
@@ -1,18 +1,20 @@
 "use strict";
 // Task 14
 console.log('> Task 14');
-let guests = ['Arsalan', 'Asad', 'Ahmed'];
+let guests = ['Arsalan', 'Daniyal', 'Khizer'];
 guests.unshift('Ahad');
 printInvitations(guests);
-function printInvitations(guests) {
-    guests.forEach(guest => console.log(`Dear ${guest}, would you like to join me for dinner`));
-}
 // Task 15
 console.log('\n> Task 15');
-let unableToAttend = 'Asad';
+guests = ['Arsalan', 'Daniyal', 'Khizer'];
+let unableToAttend = 'Daniyal';
 console.log(`${unableToAttend} can't make it to dinner.`);
 let newGuest = 'Jameel';
 // guests[guests.indexOf(unableToAttend)] = newGuest;
 // printInvitations(guests);
 guests.splice(guests.indexOf(unableToAttend), 1, newGuest);
 printInvitations(guests);
+// Function for print invitations
+function printInvitations(guests) {
+    guests.forEach(guest => console.log(`Dear ${guest}, would you like to join me for dinner`));
+}
--- task_14_and_15/index.ts
+++ task_14_and_15/index.ts
@@ -1,22 +1,27 @@
 // Task 14
 console.log('> Task 14');
-let guests: string[] = ['Arsalan', 'Asad', 'Ahmed'];
+let guests: string[] = ['Arsalan', 'Daniyal', 'Khizer'];
 guests.unshift('Ahad');
 printInvitations(guests);
 
-function printInvitations(guests:string[]){
-    guests.forEach(guest => console.log(`Dear ${guest}, would you like to join me for dinner`));
-}
 
 
 // Task 15
 console.log('\n> Task 15');
-let unableToAttend = 'Asad';
+guests = ['Arsalan', 'Daniyal', 'Khizer'];
+
+let unableToAttend:string = 'Daniyal';
 console.log(`${unableToAttend} can't make it to dinner.`)
 
-let newGuest = 'Jameel';
+let newGuest:string = 'Jameel';
 // guests[guests.indexOf(unableToAttend)] = newGuest;
 // printInvitations(guests);
 
 guests.splice(guests.indexOf(unableToAttend), 1, newGuest);
 printInvitations(guests);
+
+
+// Function for print invitations
+function printInvitations(guests:string[]){
+   guests.forEach(guest => console.log(`Dear ${guest}, would you like to join me for dinner`));
+}
