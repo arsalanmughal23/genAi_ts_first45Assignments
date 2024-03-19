b832af01234c1d576c12f34de89fffe9a36d3b53
Arsalan Mughal Tekrevol <arsalan.mughal@tekrevol.com>
2024-03-19 21:59:11 +0500

    Task 44 is Done

---
+++ task_44/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function makeSandwich(items) {
+    console.log(`Your sandwich is ready with ingredients: ${items.join(', ')}`);
+}
+makeSandwich(['chicken', 'jalapeno', 'peperoni']);
+makeSandwich(['jalapeno', 'eggs']);
+makeSandwich(['olives']);
---
+++ task_44/index.ts
@@ -0,0 +1,6 @@
+function makeSandwich(items:string[]){
+    console.log(`Your sandwich is ready with ingredients: ${items.join(', ')}`);
+}
+makeSandwich(['chicken', 'jalapeno', 'peperoni']);
+makeSandwich(['jalapeno', 'eggs']);
+makeSandwich(['olives']);
